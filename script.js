9e261ec30096a97b0be2eb0ca0309d70ef2e11b7
mkhululi <mkhululimtshali@gmail.com>
2023-03-21 21:57:45 +0200

    challenge complete

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ const arrowBtnFeaturesMobile = document.querySelector(".features-mobile-btn");
 const arrowBtnCompanyMobile = document.querySelector(".company-mobile-btn");
 
 // DESKTOP
-// const featuresDesktop = document.querySelector(".Features-Desktop-menu-item");
-// const companyDesktop = document.querySelector(".Company-Desktop-menu-item");
-// const featuresDropdownDesktop = document.querySelector(
-//   ".dropdown--features--Desktop"
-// );
-// const companyDropdownDesktop = document.querySelector(
-//   ".dropdown--company--Desktop"
-// );
-// const arrowBtnFeaturesDesktop = document.querySelector(".features-Desktop-btn");
-// const arrowBtnCompanyDesktop = document.querySelector(".company-Desktop-btn");
+const featuresDesktop = document.querySelector(".Features-desktop-menu-item");
+const companyDesktop = document.querySelector(".Company-desktop-menu-item");
+const featuresDropdownDesktop = document.querySelector(
+  ".dropdown--features--desktop"
+);
+const companyDropdownDesktop = document.querySelector(
+  ".dropdown--company--desktop"
+);
+const arrowBtnFeaturesDesktop = document.querySelector(".features-desktop-btn");
+const arrowBtnCompanyDesktop = document.querySelector(".company-desktop-btn");
 
 // EVENT LISTNERS
 mobileBtn.addEventListener("click", function () {
@@ -39,16 +39,21 @@ mobileBtn.addEventListener("click", function () {
     heroImage.style.opacity = "revert";
   }
 });
-
+// FEATURES LIST ITEM
 featuresMobile.addEventListener("click", function () {
-  // alert("features");
-  console.log(featuresDropdownMobile);
   featuresDropdownMobile.classList.toggle("hide-element");
   arrowBtnFeaturesMobile.classList.toggle("closed--dropdown");
 });
-
+featuresDesktop.addEventListener("click", function () {
+  featuresDropdownDesktop.classList.toggle("hide-element");
+  arrowBtnFeaturesDesktop.classList.toggle("closed--dropdown");
+});
+//COMPANY LIST ITEM
 companyMobile.addEventListener("click", function () {
-  console.log(companyMobile);
   companyDropdownMobile.classList.toggle("hide-element");
   arrowBtnCompanyMobile.classList.toggle("closed--dropdown");
+});
+companyDesktop.addEventListener("click", function () {
+  companyDropdownDesktop.classList.toggle("hide-element");
+  arrowBtnCompanyDesktop.classList.toggle("closed--dropdown");
 });
